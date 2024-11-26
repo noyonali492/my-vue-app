11664986f9f8cb1ce2de24ec05a991fbea75dbb9
noyonali492 <noyonahmed492@gmail.com>
2024-11-26 20:07:04 +0600

    pops

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,34 @@
   <NavBar></NavBar>
 
   <h2>Body</h2>
+  <AllFriends :friends="friends"></AllFriends>
+  <OnlineFriends :friends="friends"></OnlineFriends>
   <FooterNoyon></FooterNoyon>
 </template>
 
 <script>
 import FooterNoyon from './components/FooterNoyon.vue';
 import NavBar from './components/NavBar.vue';
+import AllFriends from './components/AllFriends.vue';
+import OnlineFriends from './components/OnlineFriends.vue';
 
 export default {
   name: 'App',
   components: {
     NavBar,
-    FooterNoyon
+    FooterNoyon,
+    AllFriends,
+    OnlineFriends
+  },
+  data(){
+    return{
+      friends:[
+            {name:'A', online:true},
+            {name:'B', online:true},
+            {name:'C', online:false},
+            {name:'D', online:true},
+        ]
+    }
   }
 }
 </script>
---
+++ src/components/AllFriends.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="allfriends">
+        <h2>All Friends</h2>
+       <div v-for="(friend,index) in friends" :key="index">
+            <h2>{{ friend.name }}</h2>
+       </div>
+    </div>
+</template>
+
+<script>
+
+export default({
+    name:'AllFriends',
+    props:['friends'],
+   data(){
+    return{
+        
+    }
+   }
+
+})
+</script>
