2a149ee849aea16250ddcd95960302922e95559f
noyonali492 <noyonahmed492@gmail.com>
2024-11-30 22:45:31 +0600

    search

--- src/components/JsonPost.vue
+++ src/components/JsonPost.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <h2 style="color: red; text-align: center;">Posts</h2><hr>
-        <div v-for="post in posts" :key="post.id">
+        
+        <input type="text" v-model="searchTerm" placeholder="search">
+        <div v-for="post in filterSearch" :key="post.id">
             <h3 style="color: yellow;">{{ post.title }}</h3>
             <p>{{ $snippet(post.body) }}</p><hr>
 
@@ -16,17 +18,29 @@ export default({
     name:'JsonPost',
    data(){
     return{
-        posts:[]
+        posts:[],
+        searchTerm: ''
     }
    },
+   computed:{
+        filterSearch(){
+           return this.posts.filter(pos=>{
+                return pos.title.match(this.searchTerm)
+            })
+        }
+   },
+
+
+
    created(){
      axios.get('https://jsonplaceholder.typicode.com/posts')
      .then(Response =>{
             this.posts = Response.data
      })
-     .catch(Error =>{
-        alert('data not found');
-     })
+     .catch((error) => {
+    console.error('Error fetching data:', error); // Log the error
+    alert('Data not found');
+    });
    }
 })
 </script>
