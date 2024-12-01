487e5d17d24aa98fc6122fb0d753f0437cc6804f
noyonali492 <noyonahmed492@gmail.com>
2024-12-01 22:00:37 +0600

    router

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <NavBar></NavBar>
-
-  <HookPart></HookPart>
-  <JsonPost></JsonPost>
-  <h2>Body</h2>
-  <AllFriends :friends="friends" @delete="deleteFriend"></AllFriends>
-  <OnlineFriends :friends="friends"></OnlineFriends>
-  <FooterNoyon></FooterNoyon>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
 </template>
 
 <script>
-import FooterNoyon from './components/FooterNoyon.vue';
-import NavBar from './components/NavBar.vue';
-import AllFriends from './components/AllFriends.vue';
-import OnlineFriends from './components/OnlineFriends.vue';
-import HookPart from './components/HookPart.vue';
-import JsonPost from './components/JsonPost.vue';
-
 export default {
-  name: 'App',
-  components: {
-    NavBar,
-    FooterNoyon,
-    AllFriends,
-    OnlineFriends,
-    HookPart,
-    JsonPost
-  },
-  data(){
-    return{
-      friends:[
-            {name:'A', online:true},
-            {name:'B', online:true},
-            {name:'C', online:false},
-            {name:'D', online:true},
-        ]
-    }
-  },
-  methods:{
-    deleteFriend(payload){
-      console.log(payload);
-      this.friends = this.friends.filter(friend =>{
-        return friend.name !== payload.name
-      })
-    }
-  }
+  name: 'App'
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
-import { createApp } from 'vue';
-import App from './App.vue';
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
 
-// Create the app instance first
-const app = createApp(App);
+Vue.config.productionTip = false
 
-// Define the global property
-app.config.globalProperties.$snippet = (val) => {
-    if (!val || typeof val !== 'string') return '';
-    return val.slice(0, 40);
-};
-
-// Mount the app
-app.mount('#app');
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
